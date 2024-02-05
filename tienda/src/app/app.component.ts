da1b86e75323a3750bef3023146cc917116b4311
Emi-ms <cabeemil86@gmail.com>
2024-02-05 14:34:28 +0100

    Update product list and component

--- tienda/src/app/app.component.ts
+++ tienda/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListaDeProductosComponent } from './lista-de-productos/lista-de-productos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListaDeProductosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- tienda/src/app/lista-de-productos/lista-de-productos.component.ts
+++ tienda/src/app/lista-de-productos/lista-de-productos.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { products } from '../products';
+import { share } from 'rxjs';
 
 @Component({
   selector: 'app-lista-de-productos',
@@ -7,6 +9,12 @@ import { Component } from '@angular/core';
   templateUrl: './lista-de-productos.component.html',
   styleUrl: './lista-de-productos.component.css'
 })
-export class ListaDeProductosComponent {
 
+export class ListaDeProductosComponent {
+  products = [...products];
+
+
+  share() {
+    window.alert('The product has been shared!');
+  }
 }
