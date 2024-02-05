41df503498a00af66e94e06e5020bba07a098d79
Emi-ms <cabeemil86@gmail.com>
2024-02-05 14:02:51 +0100

    Add lista-de-productos component and update angular.json

---
+++ tienda/src/app/lista-de-productos/lista-de-productos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-de-productos',
+  standalone: true,
+  imports: [],
+  templateUrl: './lista-de-productos.component.html',
+  styleUrl: './lista-de-productos.component.css'
+})
+export class ListaDeProductosComponent {
+
+}
