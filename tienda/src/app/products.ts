5855147dc30ea4d19147dd8f08ec5a577378f7d2
Emi-ms <cabeemil86@gmail.com>
2024-02-09 12:33:21 +0100

    Add product photos and adjust styling

--- tienda/src/app/products.ts
+++ tienda/src/app/products.ts
@@ -13,42 +13,48 @@ export const products = [
         name: 'Galaxy S21',
         price: 799,
         description: 'A large phone with one of the best screens',
-        provider: 'Samsung'
+        provider: 'Samsung',
+        photo:'assets/img/samsung21.png'
       },
       {
         id: 2,
         name: 'iPhone 12',
         price: 699,
         description: 'A great phone with one of the best cameras',
-        provider:'Apple'
+        provider:'Apple',
+        photo:'./assets/img/iphone12.png'
       },
       {
         id: 3,
         name: 'Redmi Note 10',
         price: 299,
         description: '',
-        provider:'Xiaomi'
+        provider:'Xiaomi',
+        photo:'./assets/img/note10.png'
       },
       {
         id: 4,
         name: 'Mate 40 Pro',
         price: 899,
         description: 'A large phone with one of the best screens',
-        provider:'Huawei'
+        provider:'Huawei',
+        photo:'./assets/img/mate40.jpg'
       },
       {
         id: 5,
         name: 'Pixel 4a',
         price: 299,
         description: 'A great phone with one of the best cameras',
-        provider:'Google'
+        provider:'Google',
+        photo:'./assets/img/pixel4a.png'
       },
       {
         id: 6,
         name: 'Galaxy A52',
         price: 399,
         description: 'A great phone with one of the best cameras',
-        provider:'Samsung'
+        provider:'Samsung',
+        photo:'./assets/img/a52.png'
 
       }    
 ];
